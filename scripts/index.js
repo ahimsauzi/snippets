744e95a3e24566a78463207b36fe5976a76d11c2
Uzi Ashkenazi <uzi@mediumdigitalstudio.com>
2019-10-04 08:00:16 -0700

    Bootstrap flip card.

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+/**
+ * index.js
+ * - All our useful JS goes here, awesome!
+ */
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/holder/2.9.4/holder.min.js"></script>
+document.querySelector(".card-flip").classList.toggle("flip");
+
+/* 
+ * Holder.js for demo image
+ * Just for demo purpose
+ */
+Holder.addTheme('gray', {
+  bg: '#777',
+  fg: 'rgba(255,255,255,.75)',
+  font: 'Helvetica',
+  fontweight: 'normal'
+});
